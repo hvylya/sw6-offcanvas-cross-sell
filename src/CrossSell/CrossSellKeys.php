07d0ae97a5f093bfebde2ad5ffeb5dd4ff3eecb8
hvylya <hvylya@users.noreply.github.com>
2025-08-12 09:48:13 +0200

    feat: add product custom field (hv_cross_sell_index)

---
+++ src/CrossSell/CrossSellKeys.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hv\Offcanvas\CrossSell;
+
+final class CrossSellKeys
+{
+    public const PRODUCT_CROSS_SELL_POSITION = 'hv_cross_sell_index';
+}
--- src/HvOffcanvasCrossSell.php
+++ src/HvOffcanvasCrossSell.php
@@ -4,6 +4,54 @@ declare(strict_types=1);
 
 namespace Hv\Offcanvas;
 
+use Hv\Offcanvas\Install\CustomField\ProductCrossSellIndexInstaller;
+use Psr\Container\ContainerInterface;
+use RuntimeException;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Context\UpdateContext;
+use Shopware\Core\System\CustomField\Aggregate\CustomFieldSet\CustomFieldSetCollection;
 
-class HvOffcanvasCrossSell extends Plugin {}
+class HvOffcanvasCrossSell extends Plugin
+{
+    public function install(InstallContext $installContext): void
+    {
+        $this->makeInstaller()->install($installContext->getContext());
+        parent::install($installContext);
+    }
+
+    public function update(UpdateContext $updateContext): void
+    {
+        $this->makeInstaller()->install($updateContext->getContext());
+        parent::update($updateContext);
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        if (!$uninstallContext->keepUserData()) {
+            $this->makeInstaller()->uninstall($uninstallContext->getContext());
+        }
+        parent::uninstall($uninstallContext);
+    }
+
+    private function makeInstaller(): ProductCrossSellIndexInstaller
+    {
+        $container = $this->requireContainer();
+
+        /** @var EntityRepository<CustomFieldSetCollection> $repository */
+        $repository = $container->get('custom_field_set.repository');
+
+        return new ProductCrossSellIndexInstaller($repository);
+    }
+
+    private function requireContainer(): ContainerInterface
+    {
+        if (!$this->container instanceof ContainerInterface) {
+            throw new RuntimeException('Container is not initialized yet.');
+        }
+
+        return $this->container;
+    }
+}
